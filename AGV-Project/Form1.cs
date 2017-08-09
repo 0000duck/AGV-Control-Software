c3a5e58b83115f236e22b2331f7c50f664948ddc
luissouzaf <luis.souzaf@hotmail.com>
2017-08-09 15:01:04 -0300

    Commit Tst

--- AGV-Project/Form1.cs
+++ AGV-Project/Form1.cs
@@ -16,5 +16,16 @@ namespace AGV_Project
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            System.Drawing.Pen myPen;
+            myPen = new System.Drawing.Pen(System.Drawing.Color.Tomato);
+        }
     }
 }
